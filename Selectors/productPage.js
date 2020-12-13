d10dddf5ed9f54ab043909d249931075c37f49ad
Ankur Jain <jainankur94@gmail.com>
2020-12-13 19:22:50 +0530

    applitools third test

--- Selectors/productPage.js
+++ Selectors/productPage.js
@@ -1,9 +1,27 @@
 import { Selector, t } from 'testcafe';
 
 class ProductPage {
+
     constructor() {
         this.blackColorFilter = Selector("#colors__Black")
         this.filterButton = Selector("#filterBtn")
+        this.productList = Selector("#product_grid")
+        this.firstBlackShoe = Selector('#DIV__colcolmd__210')
+        this.secondBlackShoe = Selector('#DIV__colcolmd__235')
+        this.xNight = Selector("#DIV__colcolmd__210")
+        this.productImgSection = Selector("#DIV__containerm__65")
+        this.productDescSection = Selector("#DIV__prodinfove__75")
+        this.productPriceSection = Selector("#DIV__collg__86")
+        this.shoeName = Selector("#shoe_name")
+        this.shoeImg = Selector("#shoe_img")
+        this.rating = Selector("#SPAN__rating__76")
+        this.description = Selector("#P____83")
+        this.newPrice = Selector("#new_price")
+        this.oldPrice = Selector("#old_price")
+        this.discount = Selector("#discount")
+        this.size = Selector("#DIV__row__88")
+        this.quantity = Selector("#DIV__row__98")
+        this.addToCartButton = Selector("#DIV__btnaddtoca__113")
     }
 
     async clickOnBlackFilter() {
@@ -12,6 +30,10 @@ class ProductPage {
 
     async clickOnFilterButton() {
         await t.click(this.filterButton);
+    }
+
+    async clickOnXNight() {
+        await t.click(this.xNight)
     }
 }
 
--- tests/test_1.js
+++ tests/test_1.js
@@ -1,11 +1,10 @@
-import selector from '../Selectors/ProductPage'
+import products from '../Selectors/ProductPage'
 import Eyes from '@applitools/eyes-testcafe';
 const eyes = new Eyes();
 
 fixture`AppliFashion`
     .page('http://demo.applitools.com/tlcHackathonMasterV1.html')
     .afterEach(async () => eyes.close())
-    // .after(async () => eyes.waitForResults());
 
 test('main page', async t => {
     await eyes.open({
@@ -24,17 +23,50 @@ test('filter by color', async t => {
         browser: [{ width: 1200, height: 800, name: 'firefox' }],
         t
     });
-    await selector.clickOnBlackFilter()
-    await selector.clickOnFilterButton()
+    await products.clickOnBlackFilter()
+    await products.clickOnFilterButton()
     await eyes.checkWindow({
         target: 'region',
-        selector: '#product_grid',
+        selector: products.productList,
         content: [
-            {selector: '#DIV__colcolmd__210'},
-            {selector: '#DIV__colcolmd__235'}
+            {selector: products.firstBlackShoe},
+            {selector: products.secondBlackShoe}
         ]
     });
 });
 
-
+test('product details', async t => {
+    await eyes.open({
+        appName: 'AppliFashion',
+        testName: 'Test 3',
+        browser: [{ width: 1200, height: 800, name: 'firefox' }],
+        t
+    });
+    await products.clickOnXNight()
+    await eyes.checkWindow({
+        target: 'region',
+        selector: products.productImgSection,
+        content: [
+            {selector: products.shoeName},
+            {selector: products.shoeImg},
+        ]});
+    await eyes.checkWindow({
+        target: 'region',
+        selector: products.productDescSection,
+        content: [
+            {selector: products.rating},
+            {selector: products.description},
+        ]});
+    await eyes.checkWindow({
+        target: 'region',
+        selector: products.productPriceSection,
+        content: [
+            {selector: products.newPrice},
+            {selector: products.oldPrice},
+            {selector: products.discount},
+            {selector: products.size},
+            {selector: products.quantity},
+            {selector: products.addToCartButton}
+        ]});
+});
 
